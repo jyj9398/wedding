b7716fe547026130b0893022ab93e6a0663eaac6
JaeYoon Lee <onteeae@gmail.com>
2024-07-17 22:58:16 +0900

    cron

--- hoam/hoam.py
+++ hoam/hoam.py
@@ -1,24 +1,26 @@
 from bs4 import BeautifulSoup
 
-import requests
+import requests, json
 
 url = "https://hoamwedding.com/wp-admin/admin-ajax.php"
 dates_to_check = [
-  '2025-03-09',
-  '2025-03-16',
-  '2025-03-23',
-  '2025-03-30',
-  '2025-04-06',
-  '2025-04-13',
-  '2025-04-20',
-  '2025-04-27',
-  '2025-05-11', 
-  '2025-05-18',
-  '2025-05-25',
+  {"month": "3", "date":'2025-03-09'},
+  {"month": "3", "date":'2025-03-16'},
+  {"month": "3", "date":'2025-03-23'},
+  {"month": "3", "date":'2025-03-30'},
+  {"month": "4", "date":'2025-04-06'},
+  {"month": "4", "date":'2025-04-13'},
+  {"month": "4", "date":'2025-04-20'},
+  {"month": "4", "date":'2025-04-27'},
+  {"month": "5", "date":'2025-05-11'}, 
+  {"month": "5", "date":'2025-05-18'},
+  {"month": "5", "date":'2025-05-25'},
 ]
 # iterate for dates_to_check
 result = {}
-for date in dates_to_check:
+for pair in dates_to_check:
+  month = pair['month']
+  date = pair['date']
   payload = 'action=booked_calendar_date&date={}&calendar_id=0'.format(date)
   headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
@@ -33,6 +35,10 @@ for date in dates_to_check:
 
   # Extract the text of the reservation button
   button_text = timeslot_1pm.find('span', class_='button-text').text
-  result[date] = button_text == "예약하기"
+  if button_text == "예약하기":
+    if month in result:
+      result[month].append(date)
+    else:
+      result[month] = [date]
 
-print(result)
+print(json.dumps(result, indent=4, sort_keys=True))
