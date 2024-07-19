3f60534739f8912da46aa3e4bccbef716651192d
JaeYoon Lee <onteeae@gmail.com>
2024-07-19 14:05:03 +0900

    fix

--- hoam/hoam.py
+++ hoam/hoam.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 
 import requests, json
 
+api_url = "https://api.esimsta.com/test/test"
 url = "https://hoamwedding.com/wp-admin/admin-ajax.php"
 dates_to_check = [
   {"month": "3", "date":'2025-03-09'},
@@ -21,16 +22,21 @@ result = {}
 for pair in dates_to_check:
   month = pair['month']
   date = pair['date']
-  payload = 'action=booked_calendar_date&date={}&calendar_id=0'.format(date)
+  payload = json.dumps({
+    'url': url,
+    'action': 'booked_calendar_date',
+    'date': date,
+    'calendar_id': '0'
+  })
   headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Type': 'application/json',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
   }
 
-  response = requests.request("POST", url, headers=headers, data=payload)
-  console.log(response.text)
+  response = requests.request("POST", api_url, headers=headers, data=payload)
+  print(json.loads(response.text)['data'][0])
 
-  soup = BeautifulSoup(response.text, 'html.parser')
+  soup = BeautifulSoup(json.loads(response.text)['data'][0], 'html.parser')
 
   # Find the div for the 1 PM timeslot
   timeslot_1pm = soup.find('span', string='1:00 오후').parent.parent
